3eea8206ad4c7de9dd4d660cd9182c29a3d7cf0e
Nicole Eidi <nicoleeidi1@gmail.com>
2018-05-09 00:13:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function showResults(json) {
 function forkRepo() {
   const repo = 'learn-co-curriculum/javascript-fetch-lab'
   //use fetch to fork it!
-  //POST /repos/:owner/:repo/forks
+  POST `https://api.github.com/repos/:owner/${repo}/forks`
 }
 
 function getToken() {
