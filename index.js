d24c0b7d106ee6ab92707e37e6176a6606928d50
Nicole Eidi <nicoleeidi1@gmail.com>
2018-05-09 00:39:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,12 +5,23 @@ function showIssues(json) {
 }
 
 function createIssue() {
+  const issueTitle = document.getElementById('title').value
+  const issueBody = document.getElementById('body').value
+  const postData = { title: issueTitle, body: issueBody }
+  fetch(`${baseApi}repos/${fork}/issues`, {
+    method: 'post',
+    headers: {
+      'Authorization': `token ${getToken()}`
+    },
+    body: JSON.stringify(postData)
+  }).then(resp => getIssues())
 }
 
 function showResults(json) {
 }
 
 function forkRepo() {
+  https://api.github.com/repos/learn-co-curriculum/javascript-fetch-lab/forks
     const token = ' 04ae7bc8f20238e70725123a0916a5951ab1bfe0 ';
   const repo = 'learn-co-curriculum/javascript-fetch-lab'
   //use fetch to fork it!
