19ae914d4fe991099d1265eb061ede527b84a26d
Nicole Eidi <nicoleeidi1@gmail.com>
2018-05-09 01:15:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function getIssues(data) {
     then(resp => {
       resp.json().then( data => {
         for (let i = 0; i < data.length; i++){
-          showIssue(new Issue(data[i]));
+          showIssue(   );
         }
       } )
     })
