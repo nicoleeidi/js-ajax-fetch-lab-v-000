41f1f6c5ccc5b4755f7c82f95dd4cfd1a7f42e94
Nicole Eidi <nicoleeidi1@gmail.com>
2018-05-09 00:44:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const userName = ''
 
-function getIssues() {
+function getIssues(data) {
+  fetch(`${baseApi}repos/${fork}/issues`).
+    then(resp => {
+      resp.json().then( data => {
+        for (let i = 0; i < data.length; i++){
+          displayIssue(new Issue(data[i]));
+        }
+      } )
+    })
 }
 
 function showIssues(json) {
