74d500886855cea2a587fcdb3685f8413bc8d891
Nicole Eidi <nicoleeidi1@gmail.com>
2018-05-09 02:38:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -63,15 +63,7 @@ function showForkedRepo(repo) {
 
 
 
-function getToken() {
-  const token = '';
 
-fetch('https://api.github.com/user/repos', {
-  headers: {
-    Authorization: `token ${token}`
-  }
-}).then(res => res.json()).then(json => console.log(json));
-  return ''
 
 
 const postData = {
